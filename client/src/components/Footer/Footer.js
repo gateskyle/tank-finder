b0b648126a9a9def43e701adcfb9245e2eb69eb2
Kyle G <legokyle@frontier.com>
2021-05-04 12:38:54 -0700

    Pushing final structure of repo and started on some files

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Footer.css'
+
+function Footer() {
+    return(
+        <>
+            <footer className="footer">
+                <section className="footercontainer">
+                    <span className="text-muted">
+                        Kyle Gates © 2021
+                    </span>
+                </section>
+            </footer>
+        </>
+    )
+}
+
+export default Footer;
