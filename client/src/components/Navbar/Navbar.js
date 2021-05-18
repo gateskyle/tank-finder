6ad79ad4e2ca8e4c2afa4292209f56f9d6a29892
Kyle G <legokyle@frontier.com>
2021-05-18 13:15:17 -0700

    Some deletions and changes to the project

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -4,15 +4,7 @@ import { Nav, Navbar } from 'react-bootstrap';
 function NavBar() {
     return (
         <Navbar collapseOnSelect expand="lg" sticky="top" bg="dark" variant="dark">
-            <Navbar.Brand href="/">Tank Finder</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav">
-                <Nav className="ml-auto">
-                    <Nav.Link as='h4'>Search</Nav.Link>
-                    <Nav.Link as='h4'>Saved</Nav.Link>
-                    <Nav.Link as='h4'>Profile</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
+            <Navbar.Brand>World War II Tank Finder</Navbar.Brand>
         </Navbar>
     )
 }
